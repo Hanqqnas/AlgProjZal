4505e6bf0aef3fc7e58163ed73ecbd01d6b88b1d
Hanna <hanna.kanas4@wp.pl>
2025-04-17 00:10:00 +0200

    init

--- AlgProjZal/SitoEratostenesa.cs
+++ AlgProjZal/SitoEratostenesa.cs
@@ -2,7 +2,7 @@ using System;
 using System.Collections.Generic;
 
 class SitoEratostenesa
-{
+{ 
     public static void Run()
     {
         Console.Write("Podaj liczbę całkowitą większą niż 1: ");
